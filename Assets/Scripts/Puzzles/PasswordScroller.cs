5c1c957bbcc7d1a78bd5e374a1e9fc4c13d126b4
hegeteus <45102227+hegeteus@users.noreply.github.com>
2020-02-02 10:01:41 +0200

    bffb
    fhdfhddfh

---
+++ Assets/Scripts/Puzzles/PasswordScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordScroller : MonoBehaviour
+{
+    public bool numbersOnly;
+    public ScrollRect scrollView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scrollView.GetComponentInChildren<Text>().text += "a";
+    }
+}
